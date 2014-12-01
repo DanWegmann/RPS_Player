7b44cb26bf8a2a284ecd73c6c0ab1d6f7baf4db0
daajdk <dwegmann00@gmail.com>
2014-12-01 09:40:29 -0500

    fixed some syntax errors. still need to figure out how to get the opponents last moves

--- PatAndDansRPSPlayer.py
+++ PatAndDansRPSPlayer.py
@@ -1,5 +1,5 @@
 __author__ = 'Pat McClernan and Dan Weggman'
-
+import Player
 #input
 #0 for rock
 #1 for paper
@@ -14,17 +14,27 @@ __author__ = 'Pat McClernan and Dan Weggman'
 #So our strategy is to take the least seen move
 #    and expect it to show up soon
 #   so we will play to beat that move
-class PatAndDansRPSPlayer(Player):
+class PatAndDansRPSPlayer(Player.Player):
     def __init__(self):
-        self.set_name("Pat and Dan's Player")
+        Player.Player.__init__(self)
+        self.past_moves = []
+        self.name = "Dan and Pats Player"
+        self.game_message = ""
+        self.my_player_number = 0
 
-    def play(self, past_moves):
+    def play(self):
+        self.play_helper(self, self.past_moves)
 
+    def play_helper(self, moves):
+        """
+        our player assumes that given a high number of games, all 3 different moves of opponent will be used
+        an equal number of times. Given a list of past_moves, we can counter an opponent's assumed move
+        """
         rock = 0
         paper = 0
         scissors = 0
 
-        for this_move in list(past_moves):
+        for this_move in list(moves):
             if this_move == 0:
                 rock += 1
             elif this_move == 1:
@@ -36,16 +46,41 @@ class PatAndDansRPSPlayer(Player):
             move = 0
         elif paper < scissors:
             move = 1
-        else: move = 2
+        else:
+            move = 2
 
-        print(move)
-
-        move = (move + 1 )% 3
+        move = (move + 1) % 3
 
         return move
+    """
+    gets messages from the game framework
+    Needs Work: how to get players last move from message?
+    """
+    def notify(self, message):
+        if message.is_match_end_message():
+            self.reset()
+        elif message.is_round_end_message():
+            players_in_game = message.get_players()
+            if self == players_in_game[0]:
+                self.game_message = message.get_info()
+                self.my_player_number = 1
+            elif self == players_in_game[1]:
+                self.game_message = message.get_info()
+                self.my_player_number = 2
 
-    def get_name():
+    def add_past_move(self, move):
+        """
+        adds opponents move to past moves
+        """
+        self.past_moves.append(move)
+
+    def get_name(self):
         return self.name
 
-    def set_name(name):
+    def reset(self):
+        self.past_moves = []
+        self.my_player_number = 0
+        self.game_message = ""
+
+    def set_name(self, name):
         self.name = name
