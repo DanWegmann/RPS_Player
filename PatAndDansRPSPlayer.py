bbc02b8b88c9c474f5ad80e8bd8573097d3ee213
daajdk <dwegmann00@gmail.com>
2014-12-01 13:19:50 -0500

    completed player

--- PatAndDansRPSPlayer.py
+++ PatAndDansRPSPlayer.py
@@ -1,6 +1,7 @@
-__author__ = 'Pat McClernan and Dan Weggman'
+__author__ = 'Pat McClernan and Dan Wegmann'
 import Player
-#input
+import Message
+# input
 #0 for rock
 #1 for paper
 #2 for scissors
@@ -18,14 +19,62 @@ class PatAndDansRPSPlayer(Player.Player):
     def __init__(self):
         Player.Player.__init__(self)
         self.past_moves = []
-        self.name = "Dan and Pats Player"
-        self.game_message = ""
-        self.my_player_number = 0
+        self.set_name("Dan and Pats Player")
 
     def play(self):
-        self.play_helper(self, self.past_moves)
+        return RpsPlayingStrategy.play(self.past_moves)
 
-    def play_helper(self, moves):
+    def add_past_move(self, move):
+        """
+        adds opponents move to past moves
+        """
+        self.past_moves.append(move)
+
+    def get_name(self):
+        return self.name
+
+    def notify(self, message):
+
+        # We use notifications to store opponent's moves in past rounds
+        # Process match-start and round-end messages
+        # At the start of the match, clear opponent moves history since a new match has started
+        # At the end of a round, append move to opponent's move history. Move history is used
+        # to compute the next move played.
+        if message.is_match_start_message():
+            players = message.get_players()
+            if players[0] == self or players[1] == self:
+                self.reset()
+        elif message.is_round_end_message():
+            players = message.get_players()
+            # Check if this message is for me and only then proceed
+            if (players[0] == self) or (players[1] == self):
+                # In this case, (by convention) the info is a tuple of the moves made and result
+                #  e.g. ((1, 0), (1,0)) which
+                # means player 1 played paper (1), the player 2 played rock(0) and the result was that
+                # player 1 won (got 1 point) and player 2 lost (got 0 point)
+
+                moves, result = message.get_info()
+
+                # RPS is a two person game; figure out which of the players is me
+                # and which one is the opponent
+                if players[0] == self:
+                    opponent = 1
+                else:
+                    opponent = 0
+
+                # Update opponent's past moves history
+                self.add_past_move(moves[opponent])
+
+    def reset(self):
+        self.past_moves = []
+
+    def set_name(self, name):
+        self.name = name
+
+
+class RpsPlayingStrategy(object):
+    @staticmethod
+    def play(past_moves):
         """
         our player assumes that given a high number of games, all 3 different moves of opponent will be used
         an equal number of times. Given a list of past_moves, we can counter an opponent's assumed move
@@ -34,7 +83,7 @@ class PatAndDansRPSPlayer(Player.Player):
         paper = 0
         scissors = 0
 
-        for this_move in list(moves):
+        for this_move in list(past_moves):
             if this_move == 0:
                 rock += 1
             elif this_move == 1:
@@ -52,35 +101,20 @@ class PatAndDansRPSPlayer(Player.Player):
         move = (move + 1) % 3
 
         return move
-    """
-    gets messages from the game framework
-    Needs Work: how to get players last move from message?
-    """
-    def notify(self, message):
-        if message.is_match_end_message():
-            self.reset()
-        elif message.is_round_end_message():
-            players_in_game = message.get_players()
-            if self == players_in_game[0]:
-                self.game_message = message.get_info()
-                self.my_player_number = 1
-            elif self == players_in_game[1]:
-                self.game_message = message.get_info()
-                self.my_player_number = 2
 
-    def add_past_move(self, move):
-        """
-        adds opponents move to past moves
-        """
-        self.past_moves.append(move)
 
-    def get_name(self):
-        return self.name
+# Test driver
+# Run by typing "python3 RpsPlayerExample.py"
 
-    def reset(self):
-        self.past_moves = []
-        self.my_player_number = 0
-        self.game_message = ""
+if __name__ == "__main__":
+    player = PatAndDansRPSPlayer()
+    opponent = PatAndDansRPSPlayer()
+    players = [opponent, player]
+    fakemoves = (1, 2)
+    fakeresult = (0, 1)
 
-    def set_name(self, name):
-        self.name = name
+    player.notify(Message.Message.get_match_start_message(players))
+    player.notify(Message.Message.get_round_start_message(players))
+    move = player.play()
+    print ("Move played: ", move)
+    player.notify(Message.Message.get_round_end_message(players, fakemoves, fakeresult))
